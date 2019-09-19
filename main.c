4d8a8867fc1dc6bfa262030cfd72f7edde1756ba
seyoungyook <xqd1219@naver.com>
2019-09-19 15:31:52 +0900

    02

--- main.c
+++ main.c
@@ -4,6 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("NooOOOOOOooOOOoO EE!\n");
+	
+	int x;
+	short y;
+	float z;
+	double a;
+	
+	printf("byte of ints : %d\n", sizeof(x)); 
+	printf("byte of short : %d\n", sizeof(y)); 
+	printf("byte of short : %d\n", sizeof(z));
+	printf("byte of short : %d\n", sizeof(a));
+	
 	return 0;
 }
